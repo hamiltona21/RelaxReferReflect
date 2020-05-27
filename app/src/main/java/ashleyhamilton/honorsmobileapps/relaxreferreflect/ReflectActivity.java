cd76f2c34907207a0fa62b797e7b51031256ea12
hamiltona21 <ashley_hamilton1234@yahoo.com>
2020-05-27 08:10:31 -0400

    10th commit

--- app/src/main/java/ashleyhamilton/honorsmobileapps/relaxreferreflect/ReflectActivity.java
+++ app/src/main/java/ashleyhamilton/honorsmobileapps/relaxreferreflect/ReflectActivity.java
@@ -2,7 +2,12 @@ package ashleyhamilton.honorsmobileapps.relaxreferreflect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ReflectActivity extends AppCompatActivity {
 
@@ -10,5 +15,26 @@ public class ReflectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflect);
+        Button help=findViewById(R.id.help_reflect);
+        View.OnClickListener myListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.help_reflect) {
+                    AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(ReflectActivity.this);
+                    alertDialogBuilder.setMessage("FEATURE COMING SOON: Write out your feelings in the lines below! Press the Clear button to delete your work. Customize your journal with the picking options on the top of the screen.");
+                    alertDialogBuilder.setPositiveButton("OK",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface arg0, int arg1) {
+
+                                }
+                            });
+                    AlertDialog alertDialog = alertDialogBuilder.create();
+                    alertDialog.show();
+                }
+
+            }
+        };
+        help.setOnClickListener(myListener);
     }
 }
